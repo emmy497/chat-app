18360725c68ce90f8a10bbfcb55486da0180816c
Emmanuel <emmanuelisrael2774@gmail.com>
2023-08-09 12:34:15 +0100

    First Commit

---
+++ public/js/chat.js
@@ -0,0 +1,75 @@
+const socket = io();
+
+//Elements
+
+const $messageForm = document.querySelector("#message-form");
+const $messageFormInput = document.querySelector("input");
+const $messageFormButton = document.querySelector("button");
+const $sendLocationButton = document.querySelector("#send-location");
+const $messages = document.querySelector("#messages");
+
+//Templates
+const $messageTemplate = document.querySelector("#message-template").innerHTML;
+const $locationTemplate =
+  document.querySelector("#location-template").innerHTML;
+
+$messageForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  $messageFormButton.setAttribute("disabled", "disabled");
+
+  const message = e.target.elements.message.value;
+  socket.emit("sendMessage", message, (error) => {
+    $messageFormButton.removeAttribute("disabled");
+    $messageFormInput.value = "";
+    $messageFormInput.focus();
+
+    if (error) {
+      return console.log(error);
+    }
+
+    console.log("Delivered");
+  });
+});
+
+socket.on("message", (message) => {
+  console.log(message);
+  const html = Mustache.render($messageTemplate, {
+    message,
+  });
+  $messages.insertAdjacentHTML("beforeend", html);
+});
+
+socket.on("locationMessage", (location) => {
+  console.log(location);
+  const html = Mustache.render($locationTemplate, {
+    location,
+  });
+  $messages.insertAdjacentHTML("beforeend", html);
+});
+
+document.querySelector("#send-location").addEventListener("click", () => {
+  //disable button when loaction is sent
+  $sendLocationButton.setAttribute("disabled", "disabled");
+
+  if (!navigator.geolocation) {
+    return alert("geolocation is not suported by your browser");
+  }
+
+  navigator.geolocation.getCurrentPosition((position) => {
+    socket.emit(
+      "sendLocation",
+      {
+        latitude: position.coords.latitude,
+        longitude: position.coords.longitude,
+      },
+      (error) => {
+        $sendLocationButton.removeAttribute("disabled");
+        if (error) {
+          return console.log(error);
+        }
+        console.log(`location sent`);
+      }
+    );
+  });
+});
